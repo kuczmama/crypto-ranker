dbd421f6c6415daa0ac01a4dc8e11b577fb69bdc
Mark Kuczmarski <mark@mkucz.com>
2021-12-23 17:20:42 -0800

    Make data processing service that can load data every t minutes

--- app/services/third_party_data_loading_service.rb
+++ app/services/third_party_data_loading_service.rb
@@ -1,11 +1,24 @@
 require 'json'
 require_relative '../db'
+require_relative 'load_coin_marketcap_data'
 
 class ThirdPartyDataLoadingService    
     class << self
         def load_coin_marketcap_data_from_file
             raw_file = File.read('../../data/coin-marketcap-data.json')
             JSON.parse(raw_file)["data"].each do |coin|
+                Db::coins.insert({
+                    coin_marketcap_id: coin['id'].to_i,
+                    name: coin['name'],
+                    symbol: coin['symbol'],
+                    slug: coin['slug'],
+                    rank: 0})
+            end
+        end
+
+        def load_coin_marketcap_data_from_api
+            data = LoadCoinMarketcapData.load_coin_marketcap_data
+            data.each do |coin|
                 Db::coins.insert({
                     coin_marketcap_id: coin['id'].to_i,
                     name: coin['name'],
